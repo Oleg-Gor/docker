ba4bf76145c5c7459f4afcf23c3b9a7b1c196d67
Oleg Gor <ogorbulich@gmail.com>
2021-12-03 20:59:42 +0200

    export node

--- server/data/newData.js
+++ server/data/newData.js
@@ -1,6 +1,7 @@
 const newData = JSON.stringify([
     { name: "Oleg", surname: "Gorbulich" },
     { name: "Elena", age: 23 },
-    { "oleg": 23, "1": 25, "5": 15 }
+    { "oleg": 23, "1": 25, "000": 15 }
 ])
 
+module.exports = newData;
